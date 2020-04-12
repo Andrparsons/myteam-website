9d16a2de9caa38eaacdaf2cbdfb80f90a6ffabed
andrparsons <andrparsons@gmail.com>
2020-04-12 14:33:46 -0400

    added JS to  toggle state of director cards

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+const directorBtns = document.querySelectorAll(".add-social-btn");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+});
+
+directorBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentNode.classList.toggle("director-flip");
+  });
 });
